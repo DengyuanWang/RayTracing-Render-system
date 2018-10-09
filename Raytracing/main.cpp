b4d73f1b0f220f74e1b673b3debd77a85a63c7db
王登远 <857490152@qq.com>
2018-10-08 20:23:01 -0500

    Initial Commit

---
+++ Raytracing/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Raytracing
+//
+//  Created by 王登远 on 10/8/18.
+//  Copyright © 2018 Dengyuan Wang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
